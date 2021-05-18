1d7e78a720609599df3e40d931976f42541269aa
MickaelM94 <58391020+MickaelM94@users.noreply.github.com>
2021-05-18 23:18:35 +0200

    Bouton ajout et retrait du panier

--- frontend/product.js
+++ frontend/product.js
@@ -3,20 +3,20 @@ const dataProduct = document.querySelector(".product__page");
 let id = window.location.search.substr(1);
 let data;
 // RECUPERER L'ID DU PRODUIT
-const fetchDatas = async() => {
-   data = await fetch(`http://localhost:3000/api/cameras/${id}`).then(res => res.json());
+const fetchDatas = async () => {
+  data = await fetch(`http://localhost:3000/api/cameras/${id}`).then((res) =>
+    res.json()
+  );
 };
 // SEPARATEUR DE MILLIER
-function numberWithSpaces(x){
-	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+function numberWithSpaces(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
 // AFFICHER LES DONNEES DU PRODUIT
-const showDatas = async() => {
-   await fetchDatas();
-   console.log(data);
+const showDatas = async () => {
+  await fetchDatas();
 
-   dataProduct.innerHTML = (
-         `
+  dataProduct.innerHTML = `
          <img src="${data.imageUrl}" alt="">
         <div class="product__infos">
             <p class="product__title">
@@ -36,12 +36,43 @@ const showDatas = async() => {
                 <p>
                   ${numberWithSpaces(data.price)}
                 </p>
-                <button type="button">
-                    <i class="fas fa-cart-plus"></i>
+                <button onclick="oneAddToCounter()">
+                    <i class="fas fa-plus"></i>
+                </button>
+                <button onclick="oneRemoveToCounter()">
+                    <i class="fas fa-minus"></i>
                 </button>
             </div>
         </div>
-         `
-   );
+         `;
 };
 showDatas();
+
+// == POUR LE COMPTEUR DU PANIER ==
+const getCart = document.querySelector(".cart");
+const addToCart = document.querySelector(".addToCart");
+let cartCounter = 0;
+
+// AJOUTER UNE UNITEE AU COMPTEUR
+function oneAddToCounter() {
+  // AFFICHER LE COMPTEUR APRES AJOUT
+  cartCounter++ ;
+  getCart.classList.add("cart--active");
+  getCart.innerHTML = cartCounter;
+return cartCounter;
+}
+
+// RETIRER UNE UNITEE AU COMPTEUR
+function oneRemoveToCounter() {
+  // AFFICHER LE COMPTEUR APRES SUPPRESSION
+    cartCounter-- ;
+    getCart.innerHTML = cartCounter;
+    if (cartCounter <= 0) {
+        cartCounter = 0;
+        getCart.classList.remove("cart--active");
+    }
+  return cartCounter;
+}
+
+// SYNCRONISER LA VALEUR DE LA VARIABLE DANS LE COMPTEUR VISIBLE
+
